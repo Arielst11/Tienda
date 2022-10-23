fd996f4587d6fe295ba23bb7a280bd8ec32fab8a
Arielst11 <asancheztroz@gmail.com>
2022-10-22 18:09:53 -0600

    se actualiza con la version del profe
    se actualiza el proyecto pues dejo de funcionar.

---
+++ InitializrSpringbootProject/src/main/java/com/tienda/controller/IndexController.java
@@ -0,0 +1,28 @@
+package com.tienda.controller;
+
+import com.tienda.dao.ClienteDao;
+import com.tienda.service.ClienteService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+public class IndexController {
+    
+    @Autowired
+    private ClienteService clienteService;
+    
+    @GetMapping("/")
+    public String inicio(Model model) {
+        var texto = "Estamos en semana 6";
+        model.addAttribute("mensaje", texto);
+        
+        var clientes=clienteService.getClientes();
+        
+        model.addAttribute("clientes",clientes);
+        return "index";
+    }
+}
