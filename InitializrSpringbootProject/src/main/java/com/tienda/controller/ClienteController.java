467f3c66b2d0681129d65b7b74f202e231163a94
Arielst11 <asancheztroz@gmail.com>
2022-11-22 18:02:31 -0600

    codigo de clase

--- InitializrSpringbootProject/src/main/java/com/tienda/controller/ClienteController.java
+++ InitializrSpringbootProject/src/main/java/com/tienda/controller/ClienteController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -26,10 +27,10 @@ public class ClienteController {
     
     
        @GetMapping("/cliente/buscarApellido/{apellido}")   /// posible a buscar
-    public String buscarPorApellido(Model model , String apellido) {
+    public String buscarPorApellido(Model model ,@PathVariable String apellido) {
         var clientes=clienteService.buscarPorApellido(apellido);
         model.addAttribute("clientes",clientes);
-        return "/cliente/modificar";
+        return "/cliente/listado";
     } 
     
     
